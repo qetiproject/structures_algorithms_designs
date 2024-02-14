cfdd38763c7d9cfd00cd550b22957cbca6a2ee3c
keti.khetsuriani <keti.khetsuriani@softchef.io>
2024-02-14 13:42:28 +0400

    2215. Find the Difference of Two Arrays

---
+++ Leetcode_75/2215_FindtheDifferenceofTwoArrays.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/find-the-difference-of-two-arrays/?envType=study-plan-v2&envId=leetcode-75
+
+from typing import List
+# Time: O(n1 + n2) = O(N)
+# Space: O(len(nums1) + len(nums2))
+def findDifference(nums1: List[int], nums2: List[int]) -> List[List[int]]:
+
+    set1 = set(nums1)
+    set2 = set(nums2)
+
+    difference1 = list(set1.difference(set2))
+    difference2 = list(set2.difference(set1))
+
+    result = [difference1, difference2]
+    return result
+
+print(findDifference([1,2,3],[2,4,6]))
