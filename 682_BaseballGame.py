a19d36fcd55a0ca3aa3aefd640b782c31da18ba3
keti.khetsuriani <keti.khetsuriani@softchef.io>
2024-02-16 16:26:47 +0400

    682. Baseball Game

---
+++ 682_BaseballGame.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/baseball-game/
+
+from typing import List
+
+# Time: O(N)
+# Space: O(N)
+def calPoints(operations: List[str]) -> int:
+    stack = []
+
+    for op in operations:
+        if op == "C":
+            stack.pop()
+        elif op == "D":
+            stack.append(stack[-1] * 2)
+        elif op == "+":
+            stack.append(stack[-1] + stack[-2])
+        else:
+            stack.append(int(op))
+
+
+    return sum(stack)
+
+print(calPoints(["5","2","C","D","+"]))
