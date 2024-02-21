bb1cf4852114fb98bb2a583462f063c0dac3a231
keti.khetsuriani <keti.khetsuriani@softchef.io>
2024-02-21 15:20:45 +0400

    22. Generate Parentheses

--- TopInterview_150/22_GenerateParentheses.py
+++ TopInterview_150/22_GenerateParentheses.py
@@ -2,7 +2,8 @@
 
 from typing import List
 
-
+# Time complexity: O( 2N * 2^ 2*N)= O(N * 2^N) 2^N - ხეში ნოუდების რაოდენობა, ყველა პერმუტაციის რაოდენობა, 2N - კოპირების ოპერაცია
+# Space complexity: O( 2N * 2^ 2*N)= O(N * 2^N)
 def generateParenthesis(self, n: int) -> List[str]:
     result = []
     start = 0
@@ -20,6 +21,9 @@ def generateParenthesis(self, n: int) -> List[str]:
 #         self.generateParenthesisHelper(curr + ")", start, end + 1, n, result)
 
 def generateParenthesisHelper(self, curr: int, start: int, end: int, n: int, result: List[int]) -> None:
+    # if not (end <= start <= n):
+    #     return # ამას თუ დავწერთ ქვემოთა 2 if არ დაგვჭირდება, შიდა ტანი ისედაც იმუშავებს
+
     if len(curr) == 2 * n:
         result.append("".join(curr))
         return
