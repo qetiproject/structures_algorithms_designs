748a7f8fc706fd41ea91c7bbe3392a4511bf3cca
keti.khetsuriani <keti.khetsuriani@softchef.io>
2024-02-27 16:43:14 +0400

    151. Reverse Words in a String

--- Leetcode_75/Array-String/151_ReverseWordsinaString.py
+++ Leetcode_75/Array-String/151_ReverseWordsinaString.py
@@ -2,9 +2,28 @@
 
 # Time: O(2*N), ქმნის მასივს და მისი ზომა
 # Space: O(2*N), საწყისი სტრინგის ზომა
-def reverseWords(s: str) -> str:
-    s = s.split() # O(N)
-    return " ".join(s[::-1]) # O(N) - join, reverse
+# def reverseWords(s: str) -> str:
+#     s = s.split() # O(N)
+#     return " ".join(s[::-1]) # O(N) - join, reverse
 
-print(reverseWords("the sky is blue"))
-print(reverseWords("  hello world  "))
+class Solution:
+    def reverseWords(self,  s: str) -> str:
+        result = ""
+        n = len(s)
+        i, j = 0, 0
+
+        while j < n:
+            while i < n and s[i] == " ":
+                i += 1
+            if i >= n:
+                break
+            j = i + 1
+            while j < n and s[j] != " ":
+                j += 1
+            sub = s[i:j]
+            if result == "":
+                result = sub
+            else:
+                result = sub + " " + result
+            i = j+ 1
+        return result
