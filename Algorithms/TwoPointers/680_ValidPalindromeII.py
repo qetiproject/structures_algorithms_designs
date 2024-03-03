4b7c972a84c63ae45f938f3fe2c14f1e107b85e8
keti.khetsuriani <keti.khetsuriani@softchef.io>
2024-03-03 22:18:43 +0400

    680. Valid Palindrome II

---
+++ Algorithms/TwoPointers/680_ValidPalindromeII.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/valid-palindrome-ii/
+
+# Time: O(N)
+# Space: O(N)
+def validPalindrome(s: str) -> bool:
+    i, j = 0, len(s) - 1
+
+    while i < j:
+        if s[i] != s[j]:
+            return s[i+1 : j+ 1] == s[i+1: j+1][::-1] or s[i:j] == s[i:j][::-1]
+        i += 1
+        j -= 1
+
+    return True
