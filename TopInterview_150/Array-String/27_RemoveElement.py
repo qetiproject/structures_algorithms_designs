ada4610a210e66eaafdb7cbecd4e0eed9637f179
keti.khetsuriani <keti.khetsuriani@softchef.io>
2024-03-05 21:01:41 +0400

    27. Remove Element

--- TopInterview_150/Array-String/27_RemoveElement.py
+++ TopInterview_150/Array-String/27_RemoveElement.py
@@ -5,11 +5,12 @@ from typing import List
 # Time: O(N)
 # Space: O(1)
 def removeElement(nums: List[int], val: int) -> int:
-    k, i = 0, 0
-
+    i, j = 0, 0
     while i < len(nums):
         if nums[i] != val:
-            nums[k] = nums[i]
-            k +=1
+            nums[j] = nums[i]
+            j += 1
         i += 1
-    return k
+    return j
+
+print(removeElement([3,2,2,3], 3))
