17dc3ba3c72355c8deb68b70a561b662a174f2e5
keti.khetsuriani <keti.khetsuriani@softchef.io>
2024-03-05 18:44:30 +0400

    56. Merge Intervals

---
+++ TopInterview_150/Intervals/56_MergeIntervals.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/merge-intervals/
+
+from typing import List
+
+# Time: O(N*logN)
+# Space: O(N)
+def merge(intervals: List[List[int]]) -> List[List[int]]:
+    intervals.sort(key=lambda interval: interval[0])
+    result = intervals[0]
+
+    for interval in intervals[1:]:
+        if interval[0] < result[-1][1]:
+            result[-1][1] = max(result[-1][1], interval[1])
+        else:
+            result.append(interval)
+
+    return result
