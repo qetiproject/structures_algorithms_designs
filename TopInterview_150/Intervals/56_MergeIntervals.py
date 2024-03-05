b5e94680157160f51923856b2e2d33c6903ce8be
keti.khetsuriani <keti.khetsuriani@softchef.io>
2024-03-05 18:46:48 +0400

    56. Merge Intervals

--- TopInterview_150/Intervals/56_MergeIntervals.py
+++ TopInterview_150/Intervals/56_MergeIntervals.py
@@ -6,10 +6,10 @@ from typing import List
 # Space: O(N)
 def merge(intervals: List[List[int]]) -> List[List[int]]:
     intervals.sort(key=lambda interval: interval[0])
-    result = intervals[0]
+    result = [intervals[0]]
 
     for interval in intervals[1:]:
-        if interval[0] < result[-1][1]:
+        if interval[0] <= result[-1][1]:
             result[-1][1] = max(result[-1][1], interval[1])
         else:
             result.append(interval)
