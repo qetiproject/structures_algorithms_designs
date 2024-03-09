fa19f60dd215c8ba3855b523b9e045a45b96a453
keti.khetsuriani <keti.khetsuriani@softchef.io>
2024-03-09 22:40:53 +0400

    2367. Number of Arithmetic Triplets

--- Algorithms/TwoPointers/2367_NumberofArithmeticTriplets.py
+++ Algorithms/TwoPointers/2367_NumberofArithmeticTriplets.py
@@ -1,32 +1,46 @@
 # https://leetcode.com/problems/number-of-arithmetic-triplets/
 
 
+from collections import defaultdict
 from typing import List
 
 # Time: O(N ^ 2)
 # Space: O(1)
 class Solution:
+    # def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+    #     result = 0
+    #     n = len(nums)
+
+    #     for i in range(n - 2):  # O(N)
+    #         j = i + 1  
+    #         k = j + 1  
+    #         while k < n:  # O(N)
+    #             diff1 = nums[j] - nums[i]
+    #             diff2 = nums[k] - nums[j]
+
+    #             if diff1 == diff2 == diff:
+    #                 result += 1
+    #                 j += 1  
+    #                 k += 1  
+    #             elif diff1 < diff2:
+    #                 j += 1  
+    #             else:
+    #                 k += 1 
+
+    #     return result
+
+    # Time: O(N)
+    # Space: O(N)
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
         result = 0
-        n = len(nums)
+        num_count = {}
 
-        for i in range(n - 2):  # O(N)
-            j = i + 1  
-            k = j + 1  
-            while k < n:  # O(N)
-                diff1 = nums[j] - nums[i]
-                diff2 = nums[k] - nums[j]
-
-                if diff1 == diff2 == diff:
-                    result += 1
-                    j += 1  
-                    k += 1  
-                elif diff1 < diff2:
-                    j += 1  
-                else:
-                    k += 1 
+        for num in nums:
+            if num - diff in num_count and num - 2 * diff in num_count:
+                result += num_count[num - diff]
+            num_count[num] = num_count.get(num, 0) + 1
 
         return result
 
 test = Solution()
-print(test.arithmeticTriplets([4, 5, 6, 7, 8, 9], 2))
+print(test.arithmeticTriplets([0,1,4,6,7,10], 3))
--- toDo.py
+++ toDo.py
@@ -1,11 +1,10 @@
 # Easy
 
-# https://leetcode.com/problems/design-hashmap/
-# https://leetcode.com/problems/design-hashset/
+# https://leetcode.com/problems/design-hashmap/ ვიდეო საყურებელი, კარგად არ მახსოვს
+# https://leetcode.com/problems/design-hashset/ +ვიდეო საყურებელი, კარგად არ მახსოვს
 # https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/
-# https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
 
-# https://leetcode.com/problems/number-of-arithmetic-triplets/
+# https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
 # https://leetcode.com/problems/find-lucky-integer-in-an-array/
 
 # Medium
