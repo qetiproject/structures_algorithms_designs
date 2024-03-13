c2177c5f032cddb604b2a0d3d1cfc0f124291cc0
keti.khetsuriani <keti.khetsuriani@softchef.io>
2024-03-13 22:40:57 +0400

    70. Climbing Stairs

--- Algorithms/Dynamic Programming/70_ClimbingStairs.py
+++ Algorithms/Dynamic Programming/70_ClimbingStairs.py
@@ -1,29 +1,12 @@
 # https://leetcode.com/problems/climbing-stairs/
 
 # იჭრება დროში
+# top down design
+# Time: O(2^N)
 def climbStairs(n: int) -> int:
     if n < 3:
         return n
     return climbStairs(n - 1) + climbStairs(n - 2)
-
-# Time: O(N)
-# Space: O(N)
-memo = {1: 1, 2:2}
-def climbStairs(n: int) -> int:
-    if n not in memo:
-        memo[n] = climbStairs(n - 1) + climbStairs(n-2)
-    return memo[n]
-
-# tabulation - bottom up
-# Time - O(N), ბევრჯერ თუ გამოვიძახებთ მაინც O(N)
-memo = [1, 1]
-def climbStairs(self, n: int) -> int:
-    prev, curr = 1, 1
-    for _ in range(len(memo), n + 1):
-        temp = curr
-        curr = prev + curr
-        prev = temp
-    return curr
 
 # Memorization - top down design
 # Time: O(N)
@@ -32,3 +15,25 @@ def climbStairs(self, n: int) -> int:
     if not n in self.memo:
         self.memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
     return self.memo[n]
+
+# bottom up 
+# Time: O(N), ბევრჯერ თუ გამოვიძახებთ O(N * M)
+def climbStairs(self, n: int) -> int:
+    prev, curr = 1, 1
+    for _ in range(len(self.memo), n + 1):
+        temp = curr
+        curr = temp + prev
+        prev = temp
+    return curr
+
+# tabulation - bottom up
+# Time - O(N), ბევრჯერ თუ გამოვიძახებთ მაინც O(N)
+def climbStairs(self, n: int) -> int:
+    prev, curr = 1, 1
+    for _ in range(len(memo), n + 1):
+        temp = curr
+        curr = prev + curr
+        prev = temp
+    return curr
+
+
