52b1475560330197d1e9426c14ce3f146471b750
keti.khetsuriani <keti.khetsuriani@softchef.io>
2024-03-13 16:36:56 +0400

    1854. Maximum Population Year

--- Algorithms/Prefix Sum/1854_MaximumPopulationYear.py
+++ Algorithms/Prefix Sum/1854_MaximumPopulationYear.py
@@ -2,15 +2,32 @@
 
 from typing import List
 
-# Time: O(N)
+# Time: O(N * M)
 # Space: O(N)
+# def maximumPopulation(logs: List[List[int]]) -> int:
+#     population = {}  
+#     for birth, death in logs:
+#         for year in range(birth, death):
+#             population[year] = population.get(year, 0) + 1
+
+#     max_population = max(population.values())
+#     max_years = [year for year, count in population.items() if count == max_population]
+#     return min(max_years)
+
+# Time: O(N)
+# Space: O(1)
 def maximumPopulation(logs: List[List[int]]) -> int:
-    population = {}  
+    time = [0] * 101
     for birth, death in logs:
-        for year in range(birth, death):
-            population[year] = population.get(year, 0) + 1
+        time[birth - 1950] += 1
+        time[death - 1959] -= 1
+    max_number = 0
+    max_year = 0
+    current = 0
+    for i in range(len(time)):
+        current += time[i]
+        if current > max_number:
+            max_year = i + 1950
+            max_number = current
 
-    max_population = max(population.values())
-    max_years = [year for year, count in population.items() if count == max_population]
-
-    return min(max_years)
+    return max_year
