2f5c47fdb7f38cefe3ef103b3e53d9f88ac30206
keti.khetsuriani <keti.khetsuriani@softchef.io>
2024-03-14 21:36:01 +0400

    1025. Divisor Game

---
+++ Algorithms/Dynamic Programming/1025_DivisorGame.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/divisor-game/
+
+# Time: O(N)
+# Space: O(N)
+def divisorGame(n: int) -> bool:
+    result = [False] * (n + 1)
+    result[0] = True
+    result[1] = False
+
+    for s in range(2, n+ 1):
+        for j in range(1, s):
+            print(s, j)
+            if result[s-j] == False and s % j == 0:
+                result[s] = True
+    return result[n]
