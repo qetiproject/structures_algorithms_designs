520cf734f49e72026dd7b0de613507ff838781a4
keti.khetsuriani <keti.khetsuriani@softchef.io>
2024-03-14 15:29:29 +0400

    118. Pascal's Triangle

---
+++ Algorithms/Dynamic Programming/118_Pascal'sTriangle.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/pascals-triangle/
+
+from typing import List
+
+# Time: O(N^2)
+# Space: O(N^2)
+def generate(numRows: int) -> List[List[int]]:
+    if numRows == 0:
+        return []
+
+    result = [[1]]
+
+    for _ in range(1, numRows):
+        prev_row = result[-1]
+        new_row  = [1]
+        for j in range(1, len(prev_row)):
+            new_row.append(prev_row[j - 1] + prev_row[j]) 
+        new_row.append(1)
+        result.append(new_row)
+    return result
+print(generate(5))
