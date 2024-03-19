39ff37781a15d4adeefb1e05bb887845a74ca4a1
keti.khetsuriani <keti.khetsuriani@softchef.io>
2024-03-19 21:49:36 +0400

    1544. Make The String Great

---
+++ Data Structures/Stack/1544_MakeTheStringGreat.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/make-the-string-great/
+
+# Time: O(N)
+# Space: O(N)
+class Solution:
+    def makeGood(self, s: str) -> str:
+        
+        stack = []
+        for i in range(len(s)): 
+            if stack and stack[-1] == s[i].swapcase():
+                stack.pop()
+            else:
+                stack.append(s[i])
+        
+      
+        return ''.join(stack)
